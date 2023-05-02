64170fc7f4ba048305dcbf92f90fe99ab906b28e
Anthony Manley <118389631+amanleykj@users.noreply.github.com>
2023-05-02 13:44:31 -0700

    Add files via upload

---
+++ greetings/CafeJava.java
@@ -0,0 +1,34 @@
+public class CafeJava {
+    public static void main(String[] args) {
+
+        String generalGreeting = "Welcome to Cafe Java, ";
+        String pendingMessage = ", your order will be ready shortly.";
+        String readyMessage = ", your order is ready.";
+        String displayTotalMessage = "Your total is $";
+        
+        double mochaPrice = 3.50;
+        double dripPrice = 2.50;
+        double cafeLattePrice = 3.75;
+        double shotEspressoPrice = 1.25;
+        double mochaFrappePrice = 4.75;
+        double cappucinoPrice = 3.75;
+
+        String customer1 = "Cindhuri";
+        String customer2 = "Sam";
+        String customer3 = "Jimmy";
+        String customer4 = "Noah";
+    
+        // Order completions (add yours below)
+        boolean isReadyOrder1 = false;
+    
+        // APP INTERACTION SIMULATION (Add your code for the challenges below)
+        // Example:
+        // System.out.println(generalGreeting + customer1);
+        // System.out.println(displayTotalMessage + cafeLattePrice + customer3);
+        // System.out.println(customer4 + readyMessage);
+        // System.out.println(customer2 + pendingMessage);
+        // System.out.println(customer1 + "!" + displayTotalMessage + dripPrice);
+    	// ** Your customer interaction print statements will go here ** //
+        
+    }
+}
