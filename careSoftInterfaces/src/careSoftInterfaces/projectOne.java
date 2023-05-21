de846b3adeabbe6e7b7691625536b15c6d946e48
Anthony Manley <118389631+amanleykj@users.noreply.github.com>
2023-05-21 10:56:14 -0700

    Add files via upload

---
+++ careSoftInterfaces/src/careSoftInterfaces/projectOne.java
@@ -0,0 +1,8 @@
+package careSoftInterfaces;
+
+public class projectOne {
+	public String greeting() {
+		System.out.println("Hellow wordlie!! NOW");
+		return "Hello worldies. It's time for bordlies";		
+	}
+}
---
+++ careSoftInterfaces/src/careSoftInterfaces/projectOneTest.java
@@ -0,0 +1,11 @@
+package careSoftInterfaces;
+
+public class projectOneTest {
+	public static void main(String[] args) {
+		projectOne test1 = new projectOne();
+		
+		test1.greeting();
+		System.out.println(test1.greeting());
+		
+	}
+}
